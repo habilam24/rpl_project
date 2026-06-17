e4f95c565a28198cca82c60d3b398e69b8e6fe6c
habilam24 <159699358+habilam24@users.noreply.github.com>
2026-06-17 13:36:11 +0700

    views.py

--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -1,21 +1,57 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Mahasiswa
+from .forms import MahasiswaForm
+
 
 def index(request):
     context = {
         'judul': 'Halo Mahasiswa',
-        'deskripsi': 'Belajar Django Templates dan Static Files'
+        'deskripsi': 'Contoh halaman index menggunakan Django templates dan static files.'
     }
-
     return render(request, 'mahasiswa/index.html', context)
-from django.shortcuts import render
-from .models import Mahasiswa
 
-from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/accounts/login/')
 def daftar_mahasiswa(request):
     mahasiswas = Mahasiswa.objects.all()
 
-    return render(
-        request,
-        'mahasiswa/daftar.html',
-        {'mahasiswas': mahasiswas}
-    )
+    return render(request, 'mahasiswa/daftar.html', {
+        'mahasiswas': mahasiswas
+    })
+
+
+@login_required(login_url='/accounts/login/')
+def tambah_mahasiswa(request):
+    if request.method == 'POST':
+        form = MahasiswaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Data mahasiswa berhasil ditambahkan!')
+            return redirect('daftar_mahasiswa')
+    else:
+        form = MahasiswaForm()
+    return render(request, 'mahasiswa/form.html', {'form': form})
+
+
+@login_required(login_url='/accounts/login/')
+def edit_mahasiswa(request, id):
+    mahasiswa = get_object_or_404(Mahasiswa, id=id)
+    if request.method == 'POST':
+        form = MahasiswaForm(request.POST, instance=mahasiswa)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Data mahasiswa berhasil diperbarui!')
+            return redirect('daftar_mahasiswa')
+    else:
+        form = MahasiswaForm(instance=mahasiswa)
+    return render(request, 'mahasiswa/form.html', {'form': form, 'mahasiswa': mahasiswa})
+
+
+@login_required(login_url='/accounts/login/')
+def hapus_mahasiswa(request, id):
+    mahasiswa = get_object_or_404(Mahasiswa, id=id)
+    mahasiswa.delete()
+
+    return redirect('daftar_mahasiswa')
